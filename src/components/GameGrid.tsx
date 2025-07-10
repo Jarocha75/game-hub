7d0dbf5aed1402729c9703a96d8d3465dd864366
Jarocha75 <jarocha75@gmail.com>
2025-07-10 11:39:49 +0200

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,9 +3,14 @@ import useGames from "../hooks/useGames";
 import GameCards from "./GameCards";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import type { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {
-  const { data, error, isloading } = useGames();
+interface Props {
+  selectedGenre: Genre | null;
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
@@ -14,7 +19,7 @@ const GameGrid = () => {
       spacing={5}
       padding="10px"
     >
-      {isloading &&
+      {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
             <GameCardSkeleton />
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,7 +1,14 @@
-import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from "react-icons/fa";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
 import { SiNintendo } from "react-icons/si";
 import { MdPhoneIphone } from "react-icons/md";
-import {BsGlobe} from "react-icons/bs";
+import { BsGlobe } from "react-icons/bs";
 import type { Platform } from "../hooks/useGames";
 import { HStack, Icon } from "@chakra-ui/react";
 import type { IconType } from "react-icons";
@@ -11,26 +18,24 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-    const iconMap: { [key: string]: IconType } = {
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        nintendo: SiNintendo,
-        ios: MdPhoneIphone,
-        android: FaAndroid,
-        mac: FaApple,
-        linux: FaLinux,
-        web: BsGlobe
-    };
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    mac: FaApple,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
 
   return (
     <HStack marginY={2}>
-        {platforms.map((platform) => 
-        <Icon as={iconMap[platform.slug]} color='gray.500' />
-        )}
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
     </HStack>
-    
-    
   );
 };
 
