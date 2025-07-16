bb8c3cac19d0b7e7196e80e4210ae3e58f5622f5
Jarocha75 <jarocha75@gmail.com>
2025-07-16 07:54:30 +0200

    Searching games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "89c3ace92ce54599b03b86f14829e94a",
+    key: "ee43c093255149deadc88c8e8f93d620",
   },
 });
